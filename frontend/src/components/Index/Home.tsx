210785175df7d309c806fd0bd7c8c9b7087f9963
kent2980 <kent2980@gmail.com>
2024-10-23 00:46:57 +0900

    ✨ Implement logout functionality and update Home component layout

--- frontend/src/components/Index/Home.tsx
+++ frontend/src/components/Index/Home.tsx
@@ -2,10 +2,15 @@ import { Box, Button, Heading, Stack, Text, keyframes } from "@chakra-ui/react";
 import { Link } from "@tanstack/react-router";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import useAuth from "../../hooks/useAuth";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
+  const { logout } = useAuth();
+  const handleLogout = async () => {
+    logout();
+  };
   const spin = keyframes`
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
@@ -29,7 +34,9 @@ const Home: React.FC<HomeProps> = () => {
           <Text>この情報は、株式投資においての</Text>
           <Text>参考情報としてのみ利用してください。</Text>
         </Box>
-        <Box
+        <Stack
+          direction="column"
+          spacing={6}
           height="20vh"
           display="flex"
           justifyContent="center"
@@ -44,7 +51,15 @@ const Home: React.FC<HomeProps> = () => {
           >
             利用を開始する
           </Button>
-        </Box>
+          <Button
+            colorScheme="red"
+            size="lg"
+            variant="outline"
+            onClick={handleLogout}
+          >
+            ログアウト
+          </Button>
+        </Stack>
         <Box
           display="flex"
           justifyContent="center"
