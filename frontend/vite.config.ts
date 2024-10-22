5f14b9a039a714b4d2ff00e4a22faf1dfc42dc6e
kent2980 <kent2980@gmail.com>
2024-10-23 00:25:19 +0900

    ✨ Add PWA support with vite-plugin-pwa and configure environment variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,45 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+import path from "path";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const envFiles = [".env.local", ".env"];
+envFiles.forEach((file) => {
+  dotenv.config({ path: path.resolve(__dirname, file) });
+});
+
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-})
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["public/assets/images/favicon.png"],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/i_192.png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/i_512.png",
+          },
+        ],
+      },
+    }),
+  ],
+  define: {
+    "process.env": process.env,
+  },
+});
