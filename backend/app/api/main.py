4abe1b28819090041e52b4f25a412872bfcf5349
kent2980 <kent2980@gmail.com>
2025-02-17 22:08:39 +0900

    ✨ 新しいStockWikiおよびIxFilePathエンドポイントを追加し、既存のix_non_numericエンドポイントを実装。不要なファイルを削除し、APIの構造を整理。

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -2,25 +2,30 @@ from fastapi import APIRouter
 
 from app.api.endpoints.ix_calc import router as ix_calc_router
 from app.api.endpoints.ix_check import router as ix_check_router
-from app.api.endpoints.ix_def import ix_definition
-from app.api.endpoints.ix_head import ix_head_title
-from app.api.endpoints.ix_lab import ix_label
-from app.api.endpoints.ix_non_fraction import ix_non_fraction
-from app.api.endpoints.ix_non_numeric import ix_non_numeric
-from app.api.endpoints.ix_path import ix_file_path
-from app.api.endpoints.ix_pre import ix_presentation
-from app.api.endpoints.ix_qual import ix_qualitative
-from app.api.endpoints.ix_schema import ix_schema
-from app.api.endpoints.ix_source import ix_source
-from app.api.endpoints.ix_summary import ix_summary
+from app.api.endpoints.ix_def import router as ix_def_router
+from app.api.endpoints.ix_head import router as ix_head_router
+from app.api.endpoints.ix_lab import router as ix_lab_router
+from app.api.endpoints.ix_non_fraction import router as ix_non_fraction_router
+from app.api.endpoints.ix_non_numeric import router as ix_non_numeric_router
+from app.api.endpoints.ix_path import router as ix_path_router
+from app.api.endpoints.ix_pre import router as ix_pre_router
+from app.api.endpoints.ix_qual import router as ix_qual_router
+from app.api.endpoints.ix_schema import router as ix_schema_router
+from app.api.endpoints.ix_source import router as ix_source_router
+from app.api.endpoints.ix_summary import router as ix_summary_router
+from app.api.endpoints.jpx_info import router as jpx_info_router
 from app.api.endpoints.manager import items, login, users, utils
-from app.api.endpoints.stock_wiki import jpx_stock_info, stock_wiki
+from app.api.endpoints.stock_wiki import router as stock_wiki_router
 
 api_router = APIRouter()
 
-api_router.include_router(jpx_stock_info.router, prefix="/jpx/stock_info", tags=["jpx"])
-api_router.include_router(ix_summary.router, prefix="/ix/summary", tags=["summary"])
-api_router.include_router(stock_wiki.router, prefix="/wiki", tags=["wiki"])
+api_router.include_router(
+    jpx_info_router.router, prefix="/jpx/stock_info", tags=["jpx"]
+)
+api_router.include_router(
+    ix_summary_router.router, prefix="/ix/summary", tags=["summary"]
+)
+api_router.include_router(stock_wiki_router.router, prefix="/wiki", tags=["wiki"])
 api_router.include_router(
     ix_check_router.router,
     prefix="/xbrl/check",
@@ -28,40 +33,55 @@ api_router.include_router(
     include_in_schema=False,
 )
 api_router.include_router(
-    ix_head_title.router, prefix="/xbrl", tags=["xbrl_ix_head"], include_in_schema=False
+    ix_head_router.router,
+    prefix="/xbrl",
+    tags=["xbrl_ix_head"],
+    include_in_schema=False,
 )
 api_router.include_router(
-    ix_non_numeric.router, prefix="/xbrl", tags=["xbrl_ix"], include_in_schema=False
+    ix_non_fraction_router.router,
+    prefix="/xbrl",
+    tags=["xbrl_ix"],
+    include_in_schema=False,
 )
 api_router.include_router(
-    ix_non_fraction.router, prefix="/xbrl", tags=["xbrl_ix"], include_in_schema=False
+    ix_non_numeric_router.router,
+    prefix="/xbrl",
+    tags=["xbrl_ix"],
+    include_in_schema=False,
 )
 api_router.include_router(
-    ix_label.router, prefix="/xbrl", tags=["xbrl_lab"], include_in_schema=False
+    ix_lab_router.router, prefix="/xbrl", tags=["xbrl_lab"], include_in_schema=False
 )
 api_router.include_router(
     ix_calc_router.router, prefix="/xbrl", tags=["xbrl_cal"], include_in_schema=False
 )
 api_router.include_router(
-    ix_definition.router, prefix="/def", tags=["xbrl_def"], include_in_schema=False
+    ix_def_router.router, prefix="/def", tags=["xbrl_def"], include_in_schema=False
 )
 api_router.include_router(
-    ix_presentation.router, prefix="/xbrl", tags=["xbrl_pre"], include_in_schema=False
+    ix_pre_router.router, prefix="/xbrl", tags=["xbrl_pre"], include_in_schema=False
 )
 api_router.include_router(
-    ix_source.router, prefix="/xbrl", tags=["xbrl_source"], include_in_schema=False
+    ix_source_router.router,
+    prefix="/xbrl",
+    tags=["xbrl_source"],
+    include_in_schema=False,
 )
 api_router.include_router(
-    ix_schema.router, prefix="/xbrl", tags=["xbrl_schema"], include_in_schema=False
+    ix_schema_router.router,
+    prefix="/xbrl",
+    tags=["xbrl_schema"],
+    include_in_schema=False,
 )
 api_router.include_router(
-    ix_file_path.router,
+    ix_path_router.router,
     prefix="/xbrl",
     tags=["xbrl_file_path"],
     include_in_schema=False,
 )
 api_router.include_router(
-    ix_qualitative.router,
+    ix_qual_router.router,
     prefix="/xbrl",
     tags=["xbrl_qualitative"],
     include_in_schema=False,
