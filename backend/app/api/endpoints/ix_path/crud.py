6db2501e5856eda34a3895567d16f696ceae5c2b
kent2980 <kent2980@gmail.com>
2025-02-19 04:53:25 +0900

    ✨ ix_pathエンドポイントにアイテム作成機能を追加し、コードの可読性を向上。

--- backend/app/api/endpoints/ix_path/crud.py
+++ backend/app/api/endpoints/ix_path/crud.py
@@ -1,1 +1,19 @@
 from sqlmodel import Session, select
+
+from app.models import IxFilePath
+
+from . import schema as sc
+
+
+def create_ix_file_path_item(
+    *, session: Session, item_in: sc.IxFilePathCreate
+) -> IxFilePath:
+    """
+    Create new item.
+    """
+    item = IxFilePath.model_validate(item_in)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
--- backend/app/api/endpoints/ix_path/router.py
+++ backend/app/api/endpoints/ix_path/router.py
@@ -6,6 +6,7 @@ from sqlmodel import select
 from app.api.deps import SessionDep
 from app.models import IxFilePath
 
+from . import crud
 from . import schema as sc
 
 router = APIRouter()
@@ -18,72 +19,10 @@ router = APIRouter()
 )
 def create_ix_file_path_item(
     *, session: SessionDep, item_in: sc.IxFilePathCreate
-) -> Any:
+) -> IxFilePath:
     """
     Create new item.
     """
-    item = IxFilePath.model_validate(item_in)
-    session.add(item)
-    session.commit()
-    session.refresh(item)
+    item = crud.create_ix_file_path_item(session=session, item_in=item_in)
 
     return item
-
-
-@router.get("/is/file_path/", response_model=bool)
-def is_ix_file_path_item_exists(
-    *,
-    session: SessionDep,
-    head_item_key: Optional[str] = Query(None),
-    file_path: Optional[str] = Query(None),
-) -> Any:
-    """head_item_keyまたは、ファイルパスを指定して、レコードが存在するか確認する"""
-
-    if head_item_key is None and file_path is None:
-        raise HTTPException(
-            status_code=400,
-            detail="head_item_keyまたは、ファイルパスを指定してください",
-        )
-
-    if head_item_key is not None and file_path is not None:
-        raise HTTPException(
-            status_code=400,
-            detail="head_item_keyまたは、ファイルパスのどちらか一方を指定してください",
-        )
-
-    if head_item_key is not None:
-        statement = select(IxFilePath).where(IxFilePath.head_item_key == head_item_key)
-
-    if file_path is not None:
-        statement = select(IxFilePath).where(IxFilePath.path == file_path)
-
-    result = session.exec(statement)
-    item_exists = result.first()
-
-    if item_exists:
-        return True
-
-    return False
-
-
-@router.delete("/ix/file_path/delete/", response_model=bool, include_in_schema=False)
-def delete_ix_file_path_item(
-    *, session: SessionDep, file_path: str = Query(...)
-) -> Any:
-    """
-    Delete item.
-    """
-    statement = select(IxFilePath).where(IxFilePath.path == file_path)
-    result = session.exec(statement)
-    item = result.first()
-
-    if item is None:
-        return False
-
-    try:
-        session.delete(item)
-        session.commit()
-    except Exception as e:
-        raise HTTPException(status_code=400, detail="削除に失敗しました")
-
-    return True
