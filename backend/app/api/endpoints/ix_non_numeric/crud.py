d9f918db1575f29cca0c0df198d9e8350fc3daa4
kent2980 <kent2980@gmail.com>
2025-02-19 04:50:31 +0900

    ✨ ix_non_numericエンドポイントにアイテム作成機能を追加し、エラーハンドリングを改善。コードの可読性を向上。

--- backend/app/api/endpoints/ix_non_numeric/crud.py
+++ backend/app/api/endpoints/ix_non_numeric/crud.py
@@ -1,1 +1,47 @@
+from sqlalchemy.exc import IntegrityError
 from sqlmodel import Session, select
+
+from app.models import IxNonNumeric
+
+from . import schema as sc
+
+
+def create_ix_non_numeric_item(
+    *, session: Session, item_in: sc.IxNonNumericCreate
+) -> IxNonNumeric:
+    """
+    Create new item.
+    """
+    item = IxNonNumeric.model_validate(item_in)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
+
+
+def create_ix_non_numeric_items(
+    *, session: Session, items_in: sc.IxNonNumericCreateList
+) -> str:
+    """
+    Create new items.(Insert Select ... Not Exists)
+    """
+    new_items = [IxNonNumeric.model_validate(item) for item in items_in.data]
+
+    try:
+        session.bulk_save_objects(new_items)
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        new_items = []
+        for item in items_in.data:
+            new_item = IxNonNumeric.model_validate(item)
+            session.add(new_item)
+            try:
+                session.commit()
+                session.refresh(new_item)
+                new_items.append(new_item)
+            except IntegrityError:
+                session.rollback()
+
+    return f"{len(new_items)} items created."
--- backend/app/api/endpoints/ix_non_numeric/router.py
+++ backend/app/api/endpoints/ix_non_numeric/router.py
@@ -7,6 +7,7 @@ from sqlmodel import select
 from app.api.deps import SessionDep
 from app.models import IxNonNumeric
 
+from . import crud
 from . import schema as sc
 
 router = APIRouter()
@@ -19,78 +20,23 @@ router = APIRouter()
 )
 def create_ix_non_numeric_item(
     *, session: SessionDep, item_in: sc.IxNonNumericCreate
-) -> Any:
+) -> IxNonNumeric:
     """
     Create new item.
     """
-    item = IxNonNumeric.model_validate(item_in)
-    session.add(item)
-    session.commit()
-    session.refresh(item)
+    item = crud.create_ix_non_numeric_item(session=session, item_in=item_in)
 
     return item
 
 
 @router.post("/ix/non_numeric/list/", response_model=str, include_in_schema=False)
-def create_ix_non_numeric_items_exists(
+def create_ix_non_numeric_items(
     *, session: SessionDep, items_in: sc.IxNonNumericCreateList
-) -> Any:
+) -> str:
     """
     Create new items.(Insert Select ... Not Exists)
     """
-    new_items = [IxNonNumeric.model_validate(item) for item in items_in.data]
 
-    try:
-        session.bulk_save_objects(new_items)
-        session.commit()
-    except IntegrityError:
-        session.rollback()
-        new_items = []
-        for item in items_in.data:
-            new_item = IxNonNumeric.model_validate(item)
-            session.add(new_item)
-            try:
-                session.commit()
-                session.refresh(new_item)
-                new_items.append(new_item)
-            except IntegrityError:
-                session.rollback()
+    msg = crud.create_ix_non_numeric_items(session=session, items_in=items_in)
 
-    return f"{len(new_items)} items created."
-
-
-@router.get("/ix/non_numeric/is/{source_file_id}/", response_model=bool)
-def is_ix_non_numeric_item_exists(*, session: SessionDep, source_file_id: str) -> Any:
-    """
-    Check if item exists.
-    """
-    statement = select(IxNonNumeric).where(
-        IxNonNumeric.source_file_id == source_file_id
-    )
-    result = session.exec(statement)
-    item_exists = result.first()
-
-    if item_exists:
-        return True
-
-    return False
-
-
-@router.delete("/ix/non_numeric/delete/", response_model=bool, include_in_schema=False)
-def delete_ix_non_numeric_item(
-    *, session: SessionDep, head_item_key: str = Query(...)
-) -> Any:
-    """
-    Delete item.
-    """
-    statement = select(IxNonNumeric).where(IxNonNumeric.head_item_key == head_item_key)
-    result = session.exec(statement)
-    items = result.all()
-
-    if items:
-        for item in items:
-            session.delete(item)
-        session.commit()
-        return True
-
-    return False
+    return msg
