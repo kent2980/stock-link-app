f0083acbc2a8e02f231619c95931ca62b3d7e68a
kent2980 <kent2980@gmail.com>
2025-02-19 04:44:12 +0900

    ✨ ix_labエンドポイントに複数のアイテム作成機能を追加し、エラーハンドリングを改善。コードの可読性を向上。

--- backend/app/api/endpoints/ix_lab/crud.py
+++ backend/app/api/endpoints/ix_lab/crud.py
@@ -1,1 +1,161 @@
+from sqlalchemy.exc import IntegrityError
 from sqlmodel import Session, select
+
+from app.models import IxLabelArc, IxLabelLoc, IxLabelValue
+
+from . import schema as sc
+
+
+def create_ix_label_loc_item(
+    *, session: Session, item_in: sc.IxLabelLocCreate
+) -> IxLabelLoc:
+    """
+    #### IxLabelLocの新規作成
+    - **機能**: IxLabelLocの新規作成
+    - **戻り値**: IxLabelLoc
+    - **例外**: sqlalchemy.exc.IntegrityError
+    - **param1**: session: Session
+    - **param2**: item_in: sc.IxLabelLocCreate
+    """
+
+    item = IxLabelLoc.model_validate(item_in)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
+
+
+def create_ix_label_arc_item(
+    *, session: Session, item_in: sc.IxLabelArcCreate
+) -> IxLabelArc:
+    """
+    #### IxLabelArcの新規作成
+    - **機能**: IxLabelArcの新規作成
+    - **戻り値**: IxLabelArc
+    - **例外**: sqlalchemy.exc.IntegrityError
+    - **param1**: session: Session
+    - **param2**: item_in: sc.IxLabelArcCreate
+    """
+
+    item = IxLabelArc.model_validate(item_in)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
+
+
+def create_ix_label_value_item(
+    *, session: Session, item_in: sc.IxLabelValueCreate
+) -> IxLabelValue:
+    """
+    #### IxLabelValueの新規作成
+    - **機能**: IxLabelValueの新規作成
+    - **戻り値**: IxLabelValue
+    - **例外**: sqlalchemy.exc.IntegrityError
+    - **param1**: session: Session
+    - **param2**: item_in: sc.IxLabelValueCreate
+    """
+
+    item = IxLabelValue.model_validate(item_in)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
+
+
+def create_ix_label_loc_items(
+    *, session: Session, items_in: sc.IxLabelLocCreateList
+) -> str:
+    """
+    #### 複数のIxLabelLocの新規作成
+    - **機能**: 複数のIxLabelLocの新規作成
+    - **戻り値**: str
+    - **param1**: session: Session
+    - **param2**: items_in: sc.IxLabelLocCreateList
+    """
+    new_items = [IxLabelLoc.model_validate(item) for item in items_in.data]
+
+    try:
+        session.bulk_save_objects(new_items)
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        new_items = []
+        for item in items_in.data:
+            new_item = IxLabelLoc.model_validate(item)
+            session.add(new_item)
+            try:
+                session.commit()
+                session.refresh(new_item)
+                new_items.append(new_item)
+            except IntegrityError:
+                session.rollback()  # コミット失敗時にロールバック
+
+    return f"{len(new_items)} items created."
+
+
+def create_ix_label_arc_items(
+    *, session: Session, items_in: sc.IxLabelArcCreateList
+) -> str:
+    """
+    #### 複数のIxLabelArcの新規作成
+    - **機能**: 複数のIxLabelArcの新規作成
+    - **戻り値**: str
+    - **param1**: session: Session
+    - **param2**: items_in: sc.IxLabelArcCreateList
+    """
+
+    new_items = [IxLabelArc.model_validate(item) for item in items_in.data]
+
+    try:
+        session.bulk_save_objects(new_items)
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        new_items = []
+        for item in items_in.data:
+            new_item = IxLabelArc.model_validate(item)
+            session.add(new_item)
+            try:
+                session.commit()
+                session.refresh(new_item)
+                new_items.append(new_item)
+            except IntegrityError:
+                session.rollback()
+
+    return f"{len(new_items)} items created."
+
+
+def create_ix_label_value_items(
+    *, session: Session, items_in: sc.IxLabelValueCreateList
+) -> str:
+    """
+    #### 複数のIxLabelValueの新規作成
+    - **機能**: 複数のIxLabelValueの新規作成
+    - **戻り値**: str
+    - **param1**: session: Session
+    - **param2**: items_in: sc.IxLabelValueCreateList
+    """
+
+    new_items = [IxLabelValue.model_validate(item) for item in items_in.data]
+
+    try:
+        session.bulk_save_objects(new_items)
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        new_items = []
+        for item in items_in.data:
+            new_item = IxLabelValue.model_validate(item)
+            session.add(new_item)
+            try:
+                session.commit()
+                session.refresh(new_item)
+                new_items.append(new_item)
+            except IntegrityError:
+                session.rollback()
+
+    return f"{len(new_items)} items created."
--- backend/app/api/endpoints/ix_lab/router.py
+++ backend/app/api/endpoints/ix_lab/router.py
@@ -7,6 +7,7 @@ from sqlmodel import select
 from app.api.deps import SessionDep
 from app.models import IxLabelArc, IxLabelLoc, IxLabelValue
 
+from . import crud
 from . import schema as sc
 
 router = APIRouter()
@@ -19,24 +20,15 @@ router = APIRouter()
 )
 def create_ix_label_loc_item(
     *, session: SessionDep, item_in: sc.IxLabelLocCreate
-) -> Any:
+) -> IxLabelLoc:
     """
     Create new item.
     """
 
-    source_file_id = item_in.source_file_id
-
-    statement = select(IxLabelLoc).where(IxLabelLoc.source_file_id == source_file_id)
-    result = session.exec(statement)
-    item_exists = result.first()
-
-    if item_exists:
+    try:
+        item = crud.create_ix_label_loc_item(session=session, item_in=item_in)
+    except IntegrityError:
         raise HTTPException(status_code=400, detail="Item already")
-
-    item = IxLabelLoc.model_validate(item_in)
-    session.add(item)
-    session.commit()
-    session.refresh(item)
 
     return item
 
@@ -48,24 +40,15 @@ def create_ix_label_loc_item(
 )
 def create_ix_label_arc_item(
     *, session: SessionDep, item_in: sc.IxLabelArcCreate
-) -> Any:
+) -> IxLabelArc:
     """
     Create new item.
     """
 
-    source_file_id = item_in.source_file_id
-
-    statement = select(IxLabelArc).where(IxLabelArc.source_file_id == source_file_id)
-    result = session.exec(statement)
-    item_exists = result.first()
-
-    if item_exists:
+    try:
+        item = crud.create_ix_label_arc_item(session=session, item_in=item_in)
+    except IntegrityError:
         raise HTTPException(status_code=400, detail="Item already")
-
-    item = IxLabelArc.model_validate(item_in)
-    session.add(item)
-    session.commit()
-    session.refresh(item)
 
     return item
 
@@ -77,222 +60,49 @@ def create_ix_label_arc_item(
 )
 def create_ix_label_value_item(
     *, session: SessionDep, item_in: sc.IxLabelValueCreate
-) -> Any:
+) -> IxLabelValue:
     """
     Create new item.
     """
 
-    source_file_id = item_in.source_file_id
-
-    statement = select(IxLabelValue).where(
-        IxLabelValue.source_file_id == source_file_id
-    )
-    result = session.exec(statement)
-    item_exists = result.first()
-
-    if item_exists:
+    try:
+        item = crud.create_ix_label_value_item(session=session, item_in=item_in)
+    except IntegrityError:
         raise HTTPException(status_code=400, detail="Item already")
-
-    item = IxLabelValue.model_validate(item_in)
-    session.add(item)
-    session.commit()
-    session.refresh(item)
 
     return item
 
 
 @router.post("/link/lab/loc/list/", response_model=str, include_in_schema=False)
-def create_ix_label_loc_items_exists(
+def create_ix_label_loc_items(
     *, session: SessionDep, items_in: sc.IxLabelLocCreateList
-) -> Any:
+) -> str:
     """
     Create new items.(Insert Select ... Not Exists)
     """
-    new_items = [IxLabelLoc.model_validate(item) for item in items_in.data]
-
-    try:
-        session.bulk_save_objects(new_items)
-        session.commit()
-    except IntegrityError:
-        session.rollback()
-        new_items = []
-        for item in items_in.data:
-            new_item = IxLabelLoc.model_validate(item)
-            session.add(new_item)
-            try:
-                session.commit()
-                session.refresh(new_item)
-                new_items.append(new_item)
-            except IntegrityError:
-                session.rollback()  # コミット失敗時にロールバック
-
-    return f"{len(new_items)} items created."
+    msg = crud.create_ix_label_loc_items(session=session, items_in=items_in)
+    return msg
 
 
 @router.post("/link/lab/arc/list/", response_model=str, include_in_schema=False)
-def create_ix_label_arc_items_exists(
+def create_ix_label_arc_items(
     *, session: SessionDep, items_in: sc.IxLabelArcCreateList
-) -> Any:
+) -> str:
     """
     Create new items.(Insert Select ... Not Exists)
     """
-    new_items = [IxLabelArc.model_validate(item) for item in items_in.data]
+    msg = crud.create_ix_label_arc_items(session=session, items_in=items_in)
 
-    try:
-        session.bulk_save_objects(new_items)
-        session.commit()
-    except IntegrityError:
-        session.rollback()
-        new_items = []
-        for item in items_in.data:
-            new_item = IxLabelArc.model_validate(item)
-            session.add(new_item)
-            try:
-                session.commit()
-                session.refresh(new_item)
-                new_items.append(new_item)
-            except IntegrityError:
-                session.rollback()
-
-    return f"{len(new_items)} items created."
+    return msg
 
 
 @router.post("/link/lab/value/list/", response_model=str, include_in_schema=False)
-def create_ix_label_value_items_exists(
+def create_ix_label_value_items(
     *, session: SessionDep, items_in: sc.IxLabelValueCreateList
-) -> Any:
+) -> str:
     """
     Create new items.(Insert Select ... Not Exists)
     """
-    new_items = [IxLabelValue.model_validate(item) for item in items_in.data]
+    msg = crud.create_ix_label_value_items(session=session, items_in=items_in)
 
-    try:
-        session.bulk_save_objects(new_items)
-        session.commit()
-    except IntegrityError:
-        session.rollback()
-        new_items = []
-        for item in items_in.data:
-            new_item = IxLabelValue.model_validate(item)
-            session.add(new_item)
-            try:
-                session.commit()
-                session.refresh(new_item)
-                new_items.append(new_item)
-            except IntegrityError:
-                session.rollback()
-
-    return f"{len(new_items)} items created."
-
-
-@router.get("/link/lab/loc/is/{source_file_id}/", response_model=bool)
-def get_ix_label_loc_item(*, session: SessionDep, source_file_id: str) -> Any:
-    """
-    Get item.
-    """
-    statement = select(IxLabelLoc).where(IxLabelLoc.source_file_id == source_file_id)
-    result = session.exec(statement)
-    item_exists = result.first()
-
-    if item_exists:
-        return True
-
-    return False
-
-
-@router.get("/link/lab/arc/is/{source_file_id}/", response_model=bool)
-def get_ix_label_arc_item(*, session: SessionDep, source_file_id: str) -> Any:
-    """
-    Get item.
-    """
-    statement = select(IxLabelArc).where(IxLabelArc.source_file_id == source_file_id)
-    result = session.exec(statement)
-    item_exists = result.first()
-
-    if item_exists:
-        return True
-
-    return False
-
-
-@router.get("/link/lab/value/is/{source_file_id}/", response_model=bool)
-def get_ix_label_value_item(*, session: SessionDep, source_file_id: str) -> Any:
-    """
-    Get item.
-    """
-    statement = select(IxLabelValue).where(
-        IxLabelValue.source_file_id == source_file_id
-    )
-    result = session.exec(statement)
-    item_exists = result.first()
-
-    if item_exists:
-        return True
-
-    return False
-
-
-@router.delete("/link/lab/loc/delete/", response_model=bool, include_in_schema=False)
-def delete_ix_label_loc_item(
-    *, session: SessionDep, source_file_id_list: List[str]
-) -> bool:
-    """
-    Delete item.
-    """
-    for source_file_id in source_file_id_list:
-        statement = select(IxLabelLoc).where(
-            IxLabelLoc.source_file_id == source_file_id
-        )
-        result = session.exec(statement)
-        items = result.all()
-
-        if items:
-            for item in items:
-                session.delete(item)
-            session.commit()
-
-    return True
-
-
-@router.delete("/link/lab/arc/delete/", response_model=bool, include_in_schema=False)
-def delete_ix_label_arc_item(
-    *, session: SessionDep, source_file_id_list: List[str]
-) -> bool:
-    """
-    Delete item.
-    """
-    for source_file_id in source_file_id_list:
-        statement = select(IxLabelArc).where(
-            IxLabelArc.source_file_id == source_file_id
-        )
-        result = session.exec(statement)
-        items = result.all()
-
-        if items:
-            for item in items:
-                session.delete(item)
-            session.commit()
-
-    return True
-
-
-@router.delete("/link/lab/value/delete/", response_model=bool, include_in_schema=False)
-def delete_ix_label_value_item(
-    *, session: SessionDep, source_file_id_list: List[str]
-) -> bool:
-    """
-    Delete item.
-    """
-    for source_file_id in source_file_id_list:
-        statement = select(IxLabelValue).where(
-            IxLabelValue.source_file_id == source_file_id
-        )
-        result = session.exec(statement)
-        items = result.all()
-
-        if items:
-            for item in items:
-                session.delete(item)
-            session.commit()
-
-    return True
+    return msg
