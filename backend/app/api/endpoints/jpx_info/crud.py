8d82363e711cda412576975f1a90605957953f2d
kent2980 <kent2980@gmail.com>
2025-02-19 05:25:59 +0900

    ✨ jpx_infoエンドポイントにアイテム作成機能を追加し、複数の読み取り機能を実装。コードの可読性を向上。

--- backend/app/api/endpoints/jpx_info/crud.py
+++ backend/app/api/endpoints/jpx_info/crud.py
@@ -1,1 +1,185 @@
+from typing import List, Optional
+
 from sqlmodel import Session, select
+
+from app.models import JpxStockInfo
+
+from . import schema as sc
+
+
+def create_jpx_stock_info_item(
+    *, item_in: sc.JpxStockInfoCreate, session: Session
+) -> JpxStockInfo:
+    """
+    Create new item.
+    """
+    item = JpxStockInfo.model_validate(item_in)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
+
+
+def create_jpx_stock_info_items_exists(
+    *, items_in: sc.JpxStockInfosCreateList, session: Session
+) -> List[JpxStockInfo]:
+    """
+    Create new items.(Insert Select ... Not Exists)
+    """
+
+    new_items = [JpxStockInfo.model_validate(item) for item in items_in.data]
+
+    try:
+        session.bulk_save_objects(new_items)
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        new_items = []
+        for item in items_in.data:
+            new_item = JpxStockInfo.model_validate(item)
+            session.add(new_item)
+            session.commit()
+            session.refresh(new_item)
+            new_items.append(new_item)
+
+    return new_items
+
+
+def read_jpx_stock_info_item(*, code: str, session: Session) -> Optional[JpxStockInfo]:
+    """
+    Get item by code.
+    """
+    statement = select(JpxStockInfo).where(JpxStockInfo.code == code)
+    result = session.exec(statement)
+    item = result.first()
+
+    return item
+
+
+def read_jpx_stock_info_items(*, session: Session) -> sc.JpxStockInfosPublicList:
+    """
+    Get all items.
+    """
+    statement = select(JpxStockInfo)
+    result = session.exec(statement)
+    items = result.all()
+    return sc.JpxStockInfosPublicList(data=items, count=len(items))
+
+
+def read_jpx_stock_info_items_tcs(
+    *,
+    industry_17_code: Optional[int],
+    industry_33_code: Optional[List[int]],
+    isItems: bool = True,
+    session: Session,
+    limit: int = 100,
+) -> sc.JpxStockInfosPublicList:
+    """
+    Get all items.
+    """
+    if isItems is False:
+        return sc.JpxStockInfosPublicList(data=[], count=0)
+    statement = (
+        select(JpxStockInfo)
+        .where(
+            JpxStockInfo.market_or_type.like("%内国株式%"),
+        )
+        .order_by(JpxStockInfo.code)
+    )
+    if industry_17_code:
+        statement = statement.where(JpxStockInfo.industry_17_code == industry_17_code)
+    if industry_33_code:
+        statement = statement.where(JpxStockInfo.industry_33_code.in_(industry_33_code))
+    if limit:
+        statement = statement.limit(limit)
+    result = session.exec(statement)
+    items = result.all()
+    return sc.JpxStockInfosPublicList(data=items, count=len(items))
+
+
+def read_jpx_stock_info_item_tcs(
+    *, market: str, session: Session
+) -> List[JpxStockInfo]:
+    """
+    Get item by market.
+    """
+
+    market_dict = {"pr": "プライム", "gh": "グロース", "st": "スタンダード"}
+    statement = (
+        select(JpxStockInfo)
+        .where(JpxStockInfo.market_or_type.like(f"%{market_dict[market]}%"))
+        .order_by(JpxStockInfo.code)
+    )
+    result = session.exec(statement)
+    items = result.all()
+
+    return items
+
+
+def read_jpx_stock_info_industry_names(
+    *, type: int, session: Session
+) -> sc.IndustriesList:
+    """
+    Get all industries.
+    """
+
+    if type == 17:
+        statement = (
+            select(
+                JpxStockInfo.industry_17_name.label("name"),
+                JpxStockInfo.industry_17_code.label("code"),
+            )
+            .where(
+                JpxStockInfo.industry_17_name.is_not(None),
+                JpxStockInfo.industry_17_code.is_not(None),
+            )
+            .order_by(JpxStockInfo.industry_17_code)
+            .distinct()
+        )
+    elif type == 33:
+        statement = (
+            select(
+                JpxStockInfo.industry_33_name.label("name"),
+                JpxStockInfo.industry_33_code.label("code"),
+            )
+            .where(
+                JpxStockInfo.industry_33_name.is_not(None),
+                JpxStockInfo.industry_33_code.is_not(None),
+            )
+            .order_by(JpxStockInfo.industry_33_code)
+            .distinct()
+        )
+    result = session.exec(statement)
+    items = result.all()
+    # itemsの要素からNoneを除外
+    items = [item for item in items if item[0] is not None]
+    items = [item for item in items if item[1] is not None]
+    return sc.IndustriesList(data=items)
+
+
+def read_select_industries(
+    *, industry_17_code: Optional[int], session: Session
+) -> sc.IndustriesList:
+    """
+    Get all industries.
+    """
+    statement = (
+        select(
+            JpxStockInfo.industry_33_name.label("name"),
+            JpxStockInfo.industry_33_code.label("code"),
+        )
+        .order_by(JpxStockInfo.industry_33_code)
+        .distinct()
+    )
+    if industry_17_code:
+        statement = statement.where(
+            JpxStockInfo.industry_17_code == industry_17_code,
+            JpxStockInfo.industry_33_code.is_not(None),
+        )
+    result = session.exec(statement)
+    items = result.all()
+    # itemsの要素からNoneを除外
+    items = [item for item in items if item[0] is not None]
+    items = [item for item in items if item[1] is not None]
+    return sc.IndustriesList(data=items)
--- backend/app/api/endpoints/jpx_info/router.py
+++ backend/app/api/endpoints/jpx_info/router.py
@@ -6,6 +6,7 @@ from sqlmodel import select
 from app.api.deps import SessionDep
 from app.models import JpxStockInfo
 
+from . import crud
 from . import schema as sc
 
 router = APIRouter()
@@ -14,14 +15,11 @@ router = APIRouter()
 @router.post("/", response_model=sc.JpxStockInfoCreate, include_in_schema=False)
 def create_jpx_stock_info_item(
     *, item_in: sc.JpxStockInfoCreate, session: SessionDep
-) -> Any:
+) -> JpxStockInfo:
     """
     Create new item.
     """
-    item = JpxStockInfo.model_validate(item_in)
-    session.add(item)
-    session.commit()
-    session.refresh(item)
+    item = crud.create_jpx_stock_info_item(item_in=item_in, session=session)
 
     return item
 
@@ -33,41 +31,22 @@ def create_jpx_stock_info_item(
 )
 def create_jpx_stock_info_items_exists(
     *, items_in: sc.JpxStockInfosCreateList, session: SessionDep
-) -> Any:
+) -> List[JpxStockInfo]:
     """
     Create new items.(Insert Select ... Not Exists)
     """
 
-    print("add list start")
+    items = crud.create_jpx_stock_info_items_exists(items_in=items_in, session=session)
 
-    new_items = [JpxStockInfo.model_validate(item) for item in items_in.data]
-
-    print("item add")
-
-    try:
-        session.bulk_save_objects(new_items)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        new_items = []
-        for item in items_in.data:
-            new_item = JpxStockInfo.model_validate(item)
-            session.add(new_item)
-            session.commit()
-            session.refresh(new_item)
-            new_items.append(new_item)
-
-    return new_items
+    return items
 
 
 @router.get("/code/{code}", response_model=sc.JpxStockInfoPublic)
-def read_jpx_stock_info_item(*, code: str, session: SessionDep) -> Any:
+def read_jpx_stock_info_item(*, code: str, session: SessionDep) -> JpxStockInfo:
     """
     Get item by code.
     """
-    statement = select(JpxStockInfo).where(JpxStockInfo.code == code)
-    result = session.exec(statement)
-    item = result.first()
+    item = crud.read_jpx_stock_info_item(code=code, session=session)
     if not item:
         raise HTTPException(status_code=404, detail="Item not found")
     return item
@@ -78,10 +57,10 @@ def read_jpx_stock_info_items(*, session: SessionDep) -> sc.JpxStockInfosPublicL
     """
     Get all items.
     """
-    statement = select(JpxStockInfo)
-    result = session.exec(statement)
-    items = result.all()
-    return sc.JpxStockInfosPublicList(data=items, count=len(items))
+
+    items = crud.read_jpx_stock_info_items(session=session)
+
+    return items
 
 
 @router.get("/tcs", response_model=sc.JpxStockInfosPublicList)
@@ -96,24 +75,16 @@ def read_jpx_stock_info_items_tcs(
     """
     Get all items.
     """
-    if isItems is False:
-        return sc.JpxStockInfosPublicList(data=[], count=0)
-    statement = (
-        select(JpxStockInfo)
-        .where(
-            JpxStockInfo.market_or_type.like("%内国株式%"),
-        )
-        .order_by(JpxStockInfo.code)
+
+    items = crud.read_jpx_stock_info_items_tcs(
+        industry_17_code=industry_17_code,
+        industry_33_code=industry_33_code,
+        isItems=isItems,
+        session=session,
+        limit=limit,
     )
-    if industry_17_code:
-        statement = statement.where(JpxStockInfo.industry_17_code == industry_17_code)
-    if industry_33_code:
-        statement = statement.where(JpxStockInfo.industry_33_code.in_(industry_33_code))
-    if limit:
-        statement = statement.limit(limit)
-    result = session.exec(statement)
-    items = result.all()
-    return sc.JpxStockInfosPublicList(data=items, count=len(items))
+
+    return items
 
 
 @router.get("/tcs/{market}", response_model=sc.JpxStockInfosPublicList)
@@ -127,60 +98,26 @@ def read_jpx_stock_info_item_tcs(
         raise HTTPException(
             status_code=404, detail="market is valid, select [pr, gh, st]"
         )
-    market_dict = {"pr": "プライム", "gh": "グロース", "st": "スタンダード"}
-    statement = (
-        select(JpxStockInfo)
-        .where(JpxStockInfo.market_or_type.like(f"%{market_dict[market]}%"))
-        .order_by(JpxStockInfo.code)
-    )
-    result = session.exec(statement)
-    item = result.all()
-    if not item:
+
+    items = crud.read_jpx_stock_info_item_tcs(market=market, session=session)
+
+    if len(items) == 0:
         raise HTTPException(status_code=404, detail="Item not found")
-    return sc.JpxStockInfosPublicList(data=item, count=len(item))
+
+    return sc.JpxStockInfosPublicList(data=items, count=len(items))
 
 
 @router.get("/industries/{type}", response_model=sc.IndustriesList)
-def read_jpx_stock_info_industry_names(*, type: int, session: SessionDep) -> Any:
+def read_jpx_stock_info_industry_names(
+    *, type: int, session: SessionDep
+) -> sc.IndustriesList:
     """
     Get all industries.
     """
-    if type not in [17, 33]:
-        raise HTTPException(
-            status_code=404, detail="industry type is valid, select [17, 33]"
-        )
-    if type == 17:
-        statement = (
-            select(
-                JpxStockInfo.industry_17_name.label("name"),
-                JpxStockInfo.industry_17_code.label("code"),
-            )
-            .where(
-                JpxStockInfo.industry_17_name.is_not(None),
-                JpxStockInfo.industry_17_code.is_not(None),
-            )
-            .order_by(JpxStockInfo.industry_17_code)
-            .distinct()
-        )
-    elif type == 33:
-        statement = (
-            select(
-                JpxStockInfo.industry_33_name.label("name"),
-                JpxStockInfo.industry_33_code.label("code"),
-            )
-            .where(
-                JpxStockInfo.industry_33_name.is_not(None),
-                JpxStockInfo.industry_33_code.is_not(None),
-            )
-            .order_by(JpxStockInfo.industry_33_code)
-            .distinct()
-        )
-    result = session.exec(statement)
-    items = result.all()
-    # itemsの要素からNoneを除外
-    items = [item for item in items if item[0] is not None]
-    items = [item for item in items if item[1] is not None]
-    return sc.IndustriesList(data=items)
+
+    items = crud.read_jpx_stock_info_industry_names(type=type, session=session)
+
+    return items
 
 
 @router.get(
@@ -189,26 +126,13 @@ def read_jpx_stock_info_industry_names(*, type: int, session: SessionDep) -> Any
 )
 def read_select_industries(
     *, industry_17_code: Optional[int] = Query(None), session: SessionDep
-) -> Any:
+) -> sc.IndustriesList:
     """
     Get all industries.
     """
-    statement = (
-        select(
-            JpxStockInfo.industry_33_name.label("name"),
-            JpxStockInfo.industry_33_code.label("code"),
-        )
-        .order_by(JpxStockInfo.industry_33_code)
-        .distinct()
+
+    items = crud.read_select_industries(
+        industry_17_code=industry_17_code, session=session
     )
-    if industry_17_code:
-        statement = statement.where(
-            JpxStockInfo.industry_17_code == industry_17_code,
-            JpxStockInfo.industry_33_code.is_not(None),
-        )
-    result = session.exec(statement)
-    items = result.all()
-    # itemsの要素からNoneを除外
-    items = [item for item in items if item[0] is not None]
-    items = [item for item in items if item[1] is not None]
-    return sc.IndustriesList(data=items)
+
+    return items
