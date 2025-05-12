6c6ebe23c74ebd2c89ff0a86073ddf6a5af638b6
kent2980 <kent2980@gmail.com>
2025-05-12 20:18:04 +0900

    BusinessResultTableコンポーネントの表示を改善し、WrapコンポーネントをTableコンポーネントに置き換えました。テーブル形式でのデータ表示を実装しました。

--- frontend/src/components/StockList/Tables/BusinessResultTable.tsx
+++ frontend/src/components/StockList/Tables/BusinessResultTable.tsx
@@ -1,8 +1,7 @@
 import { FinancialSummaryService } from "@/client";
-import { Box, Wrap } from "@chakra-ui/react";
+import { Box, Table } from "@chakra-ui/react";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import React from "react";
-import FinStructWrapItem from "./FinStructTable";
 
 interface BusinessResultTableProps {
   HeadItemKey: string;
@@ -19,19 +18,62 @@ const BusinessResultTable: React.FC<BusinessResultTableProps> = ({
       });
     },
   });
+  const count = data.data?.length ?? 0;
   return (
     <Box>
-      <Wrap>
-        {data?.data?.map((item, index) => (
-          <FinStructWrapItem
-            key={index}
-            label={item.label}
-            value={item.result?.curValue?.value}
-            valueScale={item.result?.curValue?.display_scale}
-            changeValue={item.result?.curChange}
-          />
-        ))}
-      </Wrap>
+      <Table.Root size="sm" minW="90vw">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader textAlign="center" minW="70px">
+              期間
+            </Table.ColumnHeader>
+            {data.data?.map((item, key) => {
+              return (
+                <Table.ColumnHeader
+                  key={key}
+                  textAlign="center"
+                  w={`${100 / count + 1}%`}
+                  colSpan={2}
+                >
+                  {item.label}
+                </Table.ColumnHeader>
+              );
+            })}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>今期実績</Table.Cell>
+            {data.data?.map((item, key) => {
+              return (
+                <>
+                  <Table.Cell key={key} textAlign="center">
+                    {item.result?.curValue?.value}
+                  </Table.Cell>
+                  <Table.Cell key={key} textAlign="center">
+                    {item.result?.curChange?.value}
+                  </Table.Cell>
+                </>
+              );
+            })}
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>前期実績</Table.Cell>
+            {data.data?.map((item, key) => {
+              return (
+                <>
+                  <Table.Cell key={key} textAlign="center">
+                    {item.result?.preValue?.value}
+                  </Table.Cell>
+                  <Table.Cell key={key} textAlign="center">
+                    {item.result?.preChange?.value}
+                  </Table.Cell>
+                </>
+              );
+            })}
+          </Table.Row>
+        </Table.Body>
+      </Table.Root>
     </Box>
   );
 };
