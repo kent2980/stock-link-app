c47a93e50d2c5ca64820f520cb5bb47521ed50f7
kent2980 <kent2980@gmail.com>
2025-07-17 20:33:58 +0900

    fix: 不要なインポートを削除し、utilsを再インポート

--- backend/app/api/endpoints/ix_summary/router.py
+++ backend/app/api/endpoints/ix_summary/router.py
@@ -7,9 +7,8 @@ from sqlmodel import select
 from app.api.deps import SessionDep
 from app.models import IxHeadTitle, IxHeadTitleSummary
 
-from . import crud
+from . import crud, utils
 from . import schema as sc
-from . import utils
 from .exceptions import HeadItemNotFound
 
 router = APIRouter()
